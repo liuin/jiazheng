0fbc83ac4dd0c8eda13ad10bd508adb972aed95e
liuin <20893791@qq.com>
2015-01-13 22:37:08 +0800

    1013

---
+++ inc/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+
+<meta name="renderer" content="webkit">
+<meta name="description" content="">
+<meta name="author" content="cuki13">
+<title><?php if(isset($title)){echo $title;} ?></title>
+<link rel="stylesheet" href="css/reset.css" />
+<link rel="stylesheet" href="css/style.css" /> 
+<script src="js/jquery.js"></script>
+<script src="js/script.js"></script>
+
+<!--[if IE 8]>
+  <link href="css/ie.css" rel="stylesheet" media="all" type="text/css" />
+<![endif]-->
+
+
+<!--[if lt IE 8]>
+  <link href="css/ie.css" rel="stylesheet" media="all" type="text/css" />
+<![endif]-->
+<!--[if IE 6]>
+  <script type="text/javascript" src="js/DD_belatedPNG_0.0.8a.js"></script>
+  <script type="text/javascript">
+    //<![CDATA[
+    DD_belatedPNG.fix('.png');
+    //]]></script>
+<![endif]-->
+</head>
+
+<body class="<?php if(isset($page)){echo $page;} ?>">
+<noscript>
+  <strong>你的浏览器似乎禁用了 JavaScript。</strong><br />您必须在浏览器中启用JavaScript才能使用本网站的功能。 
+</noscript>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php $page="home";$title="首页"; include("inc/header.php"); ?>
+
+
+
+  
+
+
+
+
+<?php include("inc/footer.php"); ?>
